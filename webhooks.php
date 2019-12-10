e36c24c2ee5f2d42d4e8566cff6760040e02a679
Gel9933 <48543594+Gel9933@users.noreply.github.com>
2019-12-10 16:56:29 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '6VE0wuvF7Xc+alylrZ8+g+CuRTnNncGTu1IoKgpFSItLuoR7K80wXpmYoM6xEQ88PRtKcd92BI4J32y81KZtp8APoAOvEi3R1W1HFv4H/vRSmFpzrVElxgWOhgat6ruqmooNQKilXXi5oX5GzMM0eAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
